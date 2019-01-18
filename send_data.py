aedb21812c640c1f3de99163209ef65a0cbfadca
Stefanos I. Tsaklidis <ctefanos.t@gmail.com>
2019-01-18 18:30:21 +0200

    infinite

--- send_data.py
+++ send_data.py
@@ -12,7 +12,7 @@ import time
 # url = 'http://192.168.1.2/sensors/catch.php'
 url = 'https://tsaklidis.gr/home/catch.php'
 
-
+time.sleep(10) # wait for wifi after reboot
 while True:
 
     data = {}
@@ -33,8 +33,6 @@ while True:
         'tempr': DS18B20.read_temp()
     })
 
-    # headers = {'content-type': 'application/json'}
-
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
                'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
@@ -53,20 +51,19 @@ while True:
         led.off()
     else:
         with open('not_sent.pkl', 'a+') as handle:
-            # file.write(json.dumps(data))
             pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
     if os.stat("not_sent.pkl").st_size > 0:  # check if file is not empty
         with open('not_sent.pkl') as f:
-            # data = json.loads(f)
-            data = pickle.load(f)
-            response = requests.post(
-                url, data=json.dumps(data), headers=headers)
+            loaded_data = pickle.load(f)
 
-            if response.status_code == 201:
-                led.off()
-                # overwrite it with emptyness
-                open('not_sent.pkl', 'w').close()
+        response = requests.post(
+            url, data=json.dumps(loaded_data), headers=headers)
+
+        if response.status_code == 201:
+            led.off()
+            # overwrite it with emptyness
+            open('not_sent.pkl', 'w').close()
 
     # print response.content
     # samples every 5 minutes
