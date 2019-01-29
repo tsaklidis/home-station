368deeec38d827c137004b839ba410397ebfbcd4
Stefanos I. Tsaklidis <ctefanos.t@gmail.com>
2019-01-29 20:38:31 +0200

    Use cron

--- README.md
+++ README.md
@@ -10,4 +10,30 @@ The data is stored on sqlite3.
 <p>Check on line https://tsaklidis.gr/home/index.php</p>
 <p>Sqlite3 file is available <a href="https://tsaklidis.gr/home/home_data.sqlite3">here</a> </p>
 
+
+> TODO:
+> <ul>
+>	<li>On data loss, save all the data saved to pickle file</li>
+>	<li>Remove 'sh: 1: modprobe: not found' from info.log (printed from DS18B20)</li>
+> </ul>
+
+
+I use the system's cron to run the script and monitor the output. To edit the cron use 
+
+```shell
+crontab -e
+```
+And run the script every 5 minutes
+```shell
+*/5 * * * * python ~/send_data.py >> ~/info.log 2>&1
+```
+
+The system cycle is:
+```mermaid
+graph TD;
+  DS18B20-->Raspberry;
+  DHT11-->Raspberry;
+  Raspberry-->Server;
+  Server-->Browser;
+```
 ![](screens/1.png)
--- send_data.py
+++ send_data.py
@@ -1,77 +1,67 @@
 import DHT11
 import DS18B20
 import datetime
-# import pickle
+import pickle
 import json
 import led
 import requests
-import time
-# import os
+import os
 
 
-# url = 'http://192.168.1.2/sensors/catch.php'
 url = 'https://tsaklidis.gr/home/catch.php'
+# url = 'http://192.168.1.2/sensors/catch.php'
 
-# wait for wifi after reboot
-# and show I am waiting
-for i in xrange(0, 9):
-    led.on()
-    time.sleep(.500)
+
+data = {}
+data['DHT11'] = []
+data['DS18B20'] = []
+bu_path = os.path.dirname(os.path.abspath(__file__))
+
+dt = datetime.datetime.now()
+data['DHT11'].append({
+    'date': dt.strftime('%d-%b-%Y'),
+    'time': dt.strftime('%H:%M'),
+    'tempr': DHT11.read_temp(),
+    'humidity': DHT11.read_humidity()
+})
+
+data['DS18B20'].append({
+    'date': dt.strftime('%d-%b-%Y'),
+    'time': dt.strftime('%H:%M'),
+    'tempr': DS18B20.read_temp()
+})
+
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+           'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+           'Accept-Encoding': 'none',
+           'Accept-Language': 'en-US,en;q=0.8',
+           'Connection': 'keep-alive',
+           'content-type': 'application/json'
+           }
+
+led.on()
+response = requests.post(
+    url, data=json.dumps(data), headers=headers)
+
+# Prevent data loss
+# To avoid problems use full path
+if response.status_code == 201:
     led.off()
-    time.sleep(.500)
+else:
+    with open(bu_path + '/not_sent.pkl', 'a+') as handle:
+        pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        print 'Data loss at: {0} '.format(dt.strftime('%d-%b-%Y %H:%M'))
 
-while True:
+if os.stat(bu_path + "/not_sent.pkl").st_size > 0:  # check if file is not empty
+    with open(bu_path + '/not_sent.pkl', 'rb') as f:
+        loaded_data = pickle.load(f)
 
-    data = {}
-    data['DHT11'] = []
-    data['DS18B20'] = []
-
-    dt = datetime.datetime.now()
-    data['DHT11'].append({
-        'date': dt.strftime('%d-%b-%Y'),
-        'time': dt.strftime('%H:%M'),
-        'tempr': DHT11.read_temp(),
-        'humidity': DHT11.read_humidity()
-    })
-
-    data['DS18B20'].append({
-        'date': dt.strftime('%d-%b-%Y'),
-        'time': dt.strftime('%H:%M'),
-        'tempr': DS18B20.read_temp()
-    })
-
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-               'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-               'Accept-Encoding': 'none',
-               'Accept-Language': 'en-US,en;q=0.8',
-               'Connection': 'keep-alive',
-               'content-type': 'application/json'
-               }
-
-    led.on()
     response = requests.post(
-        url, data=json.dumps(data), headers=headers)
+        url, data=json.dumps(loaded_data), headers=headers)
 
-    # Prevent data lose
-    # if response.status_code == 201:
-    #     led.off()
-    # else:
-    #     with open('/home/pi/Desktop/sensors/not_sent.pkl', 'a+') as handle:
-    #         pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    if response.status_code == 201:
+        led.off()
+        # overwrite it with emptyness
+        open(bu_path + '/not_sent.pkl', 'w').close()
 
-    # if os.stat("/home/pi/Desktop/sensors/not_sent.pkl").st_size > 0:  # check if file is not empty
-    #     with open('/home/pi/Desktop/sensors/not_sent.pkl', 'rb') as f:
-    #         loaded_data = pickle.load(f)
-
-    #     response = requests.post(
-    #         url, data=json.dumps(loaded_data), headers=headers)
-
-    #     if response.status_code == 201:
-    #         led.off()
-    #         # overwrite it with emptyness
-    #         open('/home/pi/Desktop/sensors/not_sent.pkl', 'w').close()
-    led.off()
-    # print response.content
-    # samples every 5 minutes
-    time.sleep(300)
