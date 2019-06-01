aa7cc89f38d4bd1f92f7a240fa0386bd7e681071
Stefanos I. Tsaklidis <ctefanos.t@gmail.com>
2019-06-01 16:31:52 +0300

    Check CPU and GPU temperatures

--- fan.py
+++ fan.py
@@ -40,7 +40,7 @@ class FanControl:
         # Inverting passed power, check circuit to investigate
         # LOW starts the fan and HIGH stops fan
         GPIO.output(self.pin, not power)
-        self.log("Fan:" + str(power) + " CPU: " + str(cpu_temp))
+        self.log("Fan:" + str(power) + " CPU: " + str(cpu_temp) + " GPU: " + str(gpu_temp)) # noqa
 
         self._power_led(power)
         self.spinning = power
@@ -49,19 +49,23 @@ class FanControl:
 fan = FanControl()
 fan.log("######### Cooler initialized #########")
 while True:
-    # res has "temp=46.2'C\n"
-    res = os.popen('/opt/vc/bin/vcgencmd measure_temp').readline()
-    cpu_temp = float((res.replace("temp=", "").replace("'C\n", "")))
+    # res_g has "temp=46.2'C\n"
+    res_g = os.popen('/opt/vc/bin/vcgencmd measure_temp').readline()
+    gpu_temp = float((res_g.replace("temp=", "").replace("'C\n", "")))
 
-    if cpu_temp > start_tmp:
+    # res_c has '43470\n'
+    res_c = os.popen('cat /sys/class/thermal/thermal_zone0/temp').readline()
+    cpu_temp = round(float((res_c.replace("\n", ""))) / 1000, 1)
+
+    if (cpu_temp > start_tmp) or (gpu_temp > start_tmp):
         if not fan.spinning:
             fan.spin(True)
     elif fan.spinning:
         fan.spin(False)
         fan.exitPin()
 
-    if cpu_temp > 50:
-        # take some time to cool the cpu
+    if (cpu_temp > 50) or (gpu_temp > 50):
+        # take some extra time to cool the cpu
         time.sleep(40)
     else:
         time.sleep(15)
