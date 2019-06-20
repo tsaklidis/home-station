d66f96c894ccb77c23ce1422b2fb51d7fc492c18
Stefanos I. Tsaklidis <ctefanos.t@gmail.com>
2019-06-20 17:46:52 +0300

    Catch data

--- store_data/catch.php
+++ store_data/catch.php
@@ -10,14 +10,15 @@
 			$data = json_decode($json_str, True);
 			$db = new SQLite3('home_data.sqlite3', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
 
-			$dht11 = $db->prepare('INSERT INTO "dht11" ("date", "time", "tempr", "humidity")
+			// The old table of dht11 is used for dht22 data
+			$dht22 = $db->prepare('INSERT INTO "dht11" ("date", "time", "tempr", "humidity")
 			    VALUES (:date_r, :time, :tempr, :humidity)');
 
-			$dht11->bindValue(':date_r', $data["DHT11"][0]["date"]);
-			$dht11->bindValue(':time', $data["DHT11"][0]["time"]);
-			$dht11->bindValue(':tempr', $data["DHT11"][0]["tempr"]);
-			$dht11->bindValue(':humidity', $data["DHT11"][0]["humidity"]);
-			$dht11->execute();
+			$dht22->bindValue(':date_r', $data["DHT22"][0]["date"]);
+			$dht22->bindValue(':time', $data["DHT22"][0]["time"]);
+			$dht22->bindValue(':tempr', $data["DHT22"][0]["tempr"]);
+			$dht22->bindValue(':humidity', $data["DHT22"][0]["humidity"]);
+			$dht22->execute();
 
 			$ds18b20 = $db->prepare('INSERT INTO "ds18b20" ("date", "time", "tempr")
 			    VALUES (:date_r2, :time2, :tempr2)');
