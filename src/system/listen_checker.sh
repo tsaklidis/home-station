8e99d757ecece47fb968de925b7aa2f0e6cfaefa
Stefanos I. Tsaklidis <ctefanos.t@gmail.com>
2020-03-28 17:50:19 +0200

    $ error

--- src/system/listen_checker.sh
+++ src/system/listen_checker.sh
@@ -2,8 +2,9 @@
 
 # run with cron like
 # */3 * * * * the/path/system/./listen_checker.sh  >> the/path/src/logs/cron_listen.log 2>&1
+# dont forget "chmod +x listen_checker.sh"
 
-RIGHT_NOW=(date +"%F_%R")
+RIGHT_NOW=$(date +"%F_%R")
 
 if [ "`systemctl show -p ActiveState --value listen`" = "active" ]
 then
