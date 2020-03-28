e9d1cee175a831ae8f11ee1447fc0de36870000a
Stefanos I. Tsaklidis <ctefanos.t@gmail.com>
2020-03-28 17:38:02 +0200

    Change listen checker.sh

--- src/system/listen_checker.sh
+++ src/system/listen_checker.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# run with cron like
+# */3 * * * * the/path/system/./listen_checker.sh  >> the/path/src/logs/cron_listen.log 2>&1
+
 RIGHT_NOW=(date +"%F_%R")
 
 if [ "`systemctl show -p ActiveState --value listen`" = "active" ]
@@ -8,7 +11,7 @@ then
     exit 0
 fi
 
-echo "$RIGHT_NOW listen (listen.service) wasnt running attempting restart" >> /home/$USER/Desktop/home-station/src/logs/listen.log
+echo "$RIGHT_NOW listen (listen.service) wasnt running attempting restart"
 
 systemctl restart listen
 
