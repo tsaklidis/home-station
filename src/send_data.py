3420132fe186da190d477e581196ddf64e445aa3
pitoula <stefanos@tsaklidis.gr>
2021-04-30 20:53:31 +0100

    garage

--- src/send_data.py
+++ src/send_data.py
@@ -1,11 +1,11 @@
 import api
 import BMP280
 import DHT22
-import DS18B20
+import wifi
 from BMP280 import get_temp as bmp_tmp
 
 try:
-    from credentials import UUIDS
+    from credentials import UUIDS, balkoni
 except ImportError, exc:
     exc.args = tuple(['%s (did you created own credentials.py?)' %
                       exc.args[0]])
@@ -14,33 +14,35 @@ except ImportError, exc:
 station = api.RemoteApi()
 
 DHT22_tmpr = {
-    "space_uuid": UUIDS['space'],
-    "sensor_uuid": UUIDS['DHT22_tmpr'],
-    "value": DHT22.read_temp(),
+    "space_uuid": balkoni['space'],
+    "sensor_uuid": balkoni['DHTXX_T'],
+    "value": DHT22.read_temp()
 }
 DHT22_hum = {
-    "space_uuid": UUIDS['space'],
-    "sensor_uuid": UUIDS['DHT22_hum'],
-    "value": DHT22.read_humidity(),
-}
-DS18B20 = {
-    "space_uuid": UUIDS['space'],
-    "sensor_uuid": UUIDS['DS18B20'],
-    "value": DS18B20.read_temp()
+    "space_uuid": balkoni['space'],
+    "sensor_uuid": balkoni['DHTXX'],
+    "value": DHT22.read_humidity()
 }
 
 BMP280 = {
-    "space_uuid": UUIDS['space'],
-    "sensor_uuid": UUIDS['BMP280'],
+    "space_uuid": balkoni['space'],
+    "sensor_uuid": balkoni['BMP280'],
     "value": BMP280.get_presure()
 }
 
 BMP280_TMP = {
-    "space_uuid": UUIDS['space'],
-    "sensor_uuid": UUIDS['BMP280_TMP'],
+    "space_uuid": balkoni['space'],
+    "sensor_uuid": balkoni['BMP280_T'],
     "value": bmp_tmp(rnd=2),
 }
+
+WIFI = {
+    "space_uuid": balkoni['space'],
+    "sensor_uuid": balkoni['WIFI'],
+    "value": wifi.get_signal()
+}
+
 # Each value overwrites the imports
-pack = [DHT22_tmpr, DHT22_hum, DS18B20, BMP280, BMP280_TMP]
+pack = [DHT22_tmpr, DHT22_hum, WIFI, BMP280, BMP280_TMP]
 
 station.send_packet(pack)
