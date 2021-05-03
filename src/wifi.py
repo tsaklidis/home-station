d6808890ca6de38da0ac3d8065c3c03d611c75f6
pitoula <stefanos@tsaklidis.gr>
2021-05-03 13:47:48 +0100

    Catch wifi exception

--- src/wifi.py
+++ src/wifi.py
@@ -3,6 +3,12 @@ import re
 
 
 def get_signal():
-    result = os.popen("iwconfig wlan0 | grep level")
+    result = os.popen("sudo iwconfig wlan0 | grep level")
     result = result.readlines()
-    return int(re.search(r'-?\d+', result[0].strip().split("Signal level=")[1]).group())
+    try:
+        value = int(
+            re.search(r'-?\d+', result[0].strip().split("Signal level=")[1]).group())
+        return value
+    except Exception as e:
+        return 0
+
