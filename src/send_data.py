b848fbaee36176907819db5207ae34f3f4e3a5da
Stefanos I. Tsaklidis (pi) <ctefanos.t@gmail.com>
2021-10-06 18:58:00 +0100

    Add blink on sensors measure

--- src/send_data.py
+++ src/send_data.py
@@ -1,13 +1,19 @@
 import api
 import DHT22
+import DHT11
 import wifi
 import system_tempr
+import led
+
+
 try:
     from BMP280 import get_temp as bmp_tmp, get_presure
+    from DS18B20 import read_temp as ds_tmp
 except IOError:
     # Temp hack for broken sensor
     bmp_tmp = lambda rnd: 0
     get_presure = lambda: 0
+    ds_tmp = lambda: 0
 
 try:
     from credentials import balkoni
@@ -18,42 +24,77 @@ except ImportError, exc:
 
 station = api.RemoteApi()
 
+
 DHT22_tmpr = {
     "space_uuid": balkoni['space'],
     "sensor_uuid": balkoni['DHTXX_T'],
     "value": DHT22.read_temp()
 }
+led.blink(3)
+
+
 DHT22_hum = {
     "space_uuid": balkoni['space'],
     "sensor_uuid": balkoni['DHTXX'],
     "value": DHT22.read_humidity()
 }
+led.blink(3)
+
+DHT11_hum = {
+    "space_uuid": balkoni['space'],
+    "sensor_uuid": balkoni['DHT11'],
+    "value": DHT11.read_humidity()
+}
+led.blink(3)
 
 BMP280 = {
     "space_uuid": balkoni['space'],
     "sensor_uuid": balkoni['BMP280'],
     "value": get_presure()
 }
+led.blink(3)
 
 BMP280_TMP = {
     "space_uuid": balkoni['space'],
     "sensor_uuid": balkoni['BMP280_T'],
     "value": bmp_tmp(rnd=2),
 }
+led.blink(3)
 
 WIFI = {
     "space_uuid": balkoni['space'],
     "sensor_uuid": balkoni['WIFI'],
     "value": wifi.get_signal()
 }
+led.blink(3)
 
 SYS_TEMPR = {
     "space_uuid": balkoni['space'],
     "sensor_uuid": balkoni['SYS_TEMPR'],
     "value": system_tempr.get_tempr()
 }
+led.blink(3)
+
+DS18B20_tmpr = {
+    "space_uuid": balkoni['space'],
+    "sensor_uuid": balkoni['DS18B20'],
+    "value": ds_tmp()
+}
+led.blink(3)
+
+
+if DHT11_hum['value'] > 100 or DHT11_hum['value'] < 0:
+    DHT11_hum['value'] = 0
+
+if DHT22_hum['value'] > 100 or DHT22_hum['value'] < 0:
+    DHT22_hum['value'] = 0
+
+if DS18B20_tmpr['value'] > 100 or DS18B20_tmpr['value'] < 0:
+    DS18B20_tmpr['value'] = 0
 
 # Some values overwrite the imports
-pack = [DHT22_tmpr, DHT22_hum, WIFI, BMP280, BMP280_TMP, SYS_TEMPR]
+pack = [DHT22_tmpr, DHT11_hum, DHT22_hum, WIFI, BMP280,
+        BMP280_TMP, SYS_TEMPR, DS18B20_tmpr]
 
 station.send_packet(pack)
+
